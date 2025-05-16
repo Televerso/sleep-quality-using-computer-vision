Fixed original dataset, Reworked processed dataset: (Added brightness normalisation, removed fragmented masks, fixed wrong cropping of images), added my own SleepTime class, got SleepState class to work, added filter for poses when object not present, added movement intensity check, fixed wrong parameters, fixed sleep metrics calculation

SleepPoseClassification/Dataset_preprocessing.py
@@ -29,12 +29,17 @@ def process_image(path, angle, scale, thresh):
     image = cv2.imread(path) # Считывает изображение
 
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # Приводит изображение к отенкам серого
-    image = bf.resize(image, 180, 135) # Подгоняет изображения к общему разрешению
-    image[-13:-1,0:30] = 0 # Удаляет водяной знак на изображениях датасета
+    image = bf.resize(image, 180*2, 135*2) # Подгоняет изображения к общему разрешению
+
+    coef = 70/(np.sum(image)/(image.shape[0]*image.shape[1]))
+    image = (image*coef).astype('uint8') # Нормализуем среднюю яркость к 70
+
+    image[-25:-1,0:60] = 0 # Удаляет водяной знак на изображениях датасета
     image = bf.rotate(image,angle,scale) # Вращает и изменяет масштаб изображения в соответствии с переданными значениями
     image = bf.threshold(image, thresh) # Приводит изображение к бинарному в соответствии с переданным значением
-    image = bf.blur(image, 3, 2) # Применяет медианный фильтр 3х3 К изображению
-    image = bf.resize(image, 64, 64) # Приводит изображение к размеру 64х64, используемому при обучении сети
+    image = bf.blur(image, 7, 2) # Применяет медианный фильтр 3х3 К изображению
+    image = bf.get_64pix_mask(image) # Приводит изображение к размеру 64х64, используемому при обучении сети
+
     return image
 
 
@@ -45,13 +50,14 @@ for pose in path_pose:
 
     i = 0
     for item in os.listdir(path):
-
+        th_low = 105
+        th_high = 135
         for var in range(0,10):
             path_item = os.path.join(path, item)
-            image = process_image(path_item,np.random.randint(0,40)-20,0.8+np.random.rand()*0.4,np.random.randint(95,145))
+            image = process_image(path_item,np.random.randint(0,40)-20,1,np.random.randint(th_low,th_high))
 
-            if np.sum(image) < 0.3*256*(image.shape[0]*image.shape[1]):
-                image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+            if np.sum(image) < 0.35*3*256*(image.shape[0]*image.shape[1]) and np.sum(image) > 0.1*3*256*(image.shape[0]*image.shape[1]):
+
                 cv2.imwrite(os.path.join(f"Dataset/Train/{pose}/{i}.png"), image)
                 i+=1
 
@@ -66,10 +72,10 @@ for pose in path_pose:
 
         for var in range(0,10):
             path_item = os.path.join(path, item)
-            image = process_image(path_item,np.random.randint(0,30)-15,0.85+np.random.rand()*0.3,np.random.randint(100,140))
+            image = process_image(path_item,np.random.randint(0,30)-15,1,np.random.randint(90,130))
 
-            if np.sum(image) < 0.3*256*(image.shape[0] * image.shape[1]):
-                image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+            if np.sum(image) < 0.35*3*256*(image.shape[0]*image.shape[1]) and np.sum(image) > 0.1*3*256*(image.shape[0]*image.shape[1]):
+
                 cv2.imwrite(os.path.join(f"Dataset/Test/{pose}/{i}.png"), image)
                 i += 1
     print(pose)

SleepState/SleepState.py
@@ -7,42 +7,93 @@ import numpy as np
 from FrameClass.FrameClass import Frame
 from SleepTranscription import SleepTranscription
 from basic_functions import basic_functions as bf
-import time
-from datetime import datetime
 from collections import Counter
 
-class sleep_time():
-    def __init__(self, year, month, day, hour, minute, second):
-        self.year = year
-        self.month = month
-        self.day = day
-        self.hour = hour
-        self.minute = minute
-        self.second = second
+class SleepTime:
+    def __init__(self, hour, minute, second):
+        self.hour = int(hour)
+        self.minute = int(minute)
+        self.second = int(second)
+
+        self.in_seconds = second + minute*60 + hour*3600
+
+    def __add__(self, other):
+        hour, minute, second, in_seconds = 0,0,0,0
+        if type(other) is SleepTime:
+            hour = (other.hour + self.hour) % 24
+            minute = (other.minute + self.minute) % 60
+            second = (other.second + self.second) % 60
+
+        elif type(other) is int or type(other) is float:
+            in_seconds = (other + self.in_seconds) % (3600*24)
+
+            hour = self.in_seconds // 3600
+            minute = (self.in_seconds // 60) % 60
+            second = self.in_seconds % 3600
+        else:
+            assert "Wrong datatype!"
+
+        return SleepTime(hour, minute, second)
+
+    def __sub__(self, other):
+        hour, minute, second, in_seconds = 0,0,0,0
+        if type(other) is SleepTime:
+            hour = (self.hour - other.hour) % 24
+            minute = (self.minute -  other.minute) % 60
+            second = (self.second - other.second) % 60
+
+        elif type(other) is int or type(other) is float:
+            in_seconds = (self.in_seconds - other) % (3600*24)
+
+            hour = self.in_seconds // 3600
+            minute = (self.in_seconds // 60) % 60
+            second = self.in_seconds % 3600
+        else:
+            assert "Wrong datatype!"
+
+        return SleepTime(hour, minute, second)
+
+    def __str__(self):
+        res_str = ''
+        res_str += self.hour
+        res_str += ':'
+        res_str += self.minute
+        res_str += ':'
+        res_str += self.second
+        res_str += " - ("
+        res_str += self.in_seconds
+        res_str += "s)"
+
+        return res_str
 
     def to_struct_time(self):
         pass
 
 class SleepState:
-    def __init__(self, pose_list, is_moved, is_present, starting_time, framerate = 1, epoch_len = 30):
-        self.is_moved = is_moved
+    def __init__(self, pose_list, movement_intensity, is_present, starting_time, framerate = 1, epoch_len = 30, movement_threshhold = 0.01):
+        self.movement_intensity = movement_intensity
         self.is_present = is_present
         self.poses = pose_list
 
-        self.stage_array = np.array((len(is_moved)))
+        self.stage_array = np.array((len(movement_intensity)))
         self.stage_dict = dict()
         self.epoch_len = epoch_len
 
         self.starting_time = starting_time
-        self.sleep_duration_in_sec = starting_time - len(is_moved)/framerate
+        self.record_len_in_sec = int(len(movement_intensity)/framerate)
 
-    def _count_movments(self):
+        self.calc_sleep_stage(movement_threshhold)
+        self._get_stage_dict()
+
+        self.sleep_duration_in_sec = self.record_len_in_sec - int(len(self.stage_array[self.stage_array == "WAKE"])/framerate)
+
+    def _count_movments(self, threshhold):
         mov_dict = dict()
 
         prev_state = False
-        for i in range(len(self.is_moved)):
+        for i in range(len(self.movement_intensity)):
 
-            if self.is_moved[i]:
+            if self.movement_intensity[i] > threshhold:
                 if not prev_state:
                     mov_dict[i] = True
                 prev_state = True
@@ -52,20 +103,21 @@ class SleepState:
                 prev_state = False
         return mov_dict
 
-    def calc_sleep_stage(self):
+    def calc_sleep_stage(self, threshhold):
         epoch_len = self.epoch_len
         # Заполняем массив данных значениями соответствующими небыстрому сну
-        self.stage_array = np.array(["NREM" for i in range(len(self.is_moved))], dtype=str)
+        self.stage_array = np.array(["NREM" for i in range(len(self.movement_intensity))], dtype=str)
 
         # На промежутках
         for i in range(0, self.stage_array.shape[0], epoch_len):
-            left_border = i-epoch_len//2 if (i-epoch_len//2) < 0 else i
-            right_border = i+epoch_len//2 if (i+epoch_len//2) < 0 else i
+            left_border = i-epoch_len//2 if (i-epoch_len//2) > 0 else i
+            right_border = i+epoch_len//2 if (i+epoch_len//2) < len(self.stage_array) else len(self.stage_array)
 
             if not min(self.is_present[left_border:right_border]):
                 self.stage_array[left_border:right_border] = "WAKE"
-
-            elif max(self.is_moved[left_border:right_border]):
+            elif max(self.movement_intensity[left_border:right_border]) > threshhold:
+                self.stage_array[left_border:right_border] = "WAKE"
+            elif max(self.movement_intensity[left_border:right_border]) <= threshhold:
                 self.stage_array[left_border:right_border] = "REM"
 
         return self.stage_array
@@ -93,17 +145,17 @@ class SleepState:
         return self.stage_dict
 
     def _calc_TST(self):
-        return self.sleep_duration_in_sec
+        return self.sleep_duration_in_sec/3600
 
     def _calc_REM(self):
-        return np.sum(self.stage_array[:]=="REM")
+        return (np.sum(self.stage_array[:]=="REM") / len(self.stage_array[self.stage_array != "WAKE"])) * self._calc_TST()
 
     def _calc_N_REM(self):
         vals = self.stage_dict.values()
         return Counter(vals)["REM"]
 
     def _calc_WAKE(self):
-        return np.sum(self.stage_array[:]=="WAKE")
+        return (np.sum(self.stage_array[:]=="WAKE") / len(self.stage_array)) * (self.record_len_in_sec / 3600)
 
     def _calc_N_WAKE(self):
         vals = self.stage_dict.values()
@@ -111,18 +163,22 @@ class SleepState:
 
 
     def _calc_NREM(self):
-        return np.sum(self.stage_array[:]=="NREM")
+        return (np.sum(self.stage_array[:]=="NREM") / len(self.stage_array[self.stage_array != "WAKE"])) * (self.record_len_in_sec / 3600)
 
     def _count_Pose(self):
-        counts, vals = np.unique(self.poses, return_counts=True)
-        return np.max(counts)
+        vals, counts = np.unique(self.poses[self.stage_array != "WAKE"], return_counts=True)
 
-    def get_sleeping_score(self, Am=8.5, Ap=8.5, Aw = 2 , alpha = 1, beta = 1, gamma = 0.5):
+        return np.max(counts)/ len(self.poses[self.stage_array != "WAKE"]) * (self.record_len_in_sec / 3600)
+
+    def get_sleeping_score(self, Am=8.5, Ap=8.5, Aw = 2 , alpha = 1, beta = 0.01, gamma = 0.5):
+        if min(np.asarray(self.stage_array) == "WAKE"):
+            return (0,0,0)
+
         TST = self._calc_TST()
         TR = self._calc_REM()
         TW = self._calc_WAKE()
         TNR = self._calc_NREM()
-        A = self._calc_N_WAKE()
+        A = self._calc_N_WAKE()-2
         NR = self._calc_N_REM()
         P = self._count_Pose() # most_frequent_pose
 
@@ -133,6 +189,7 @@ class SleepState:
 
         scores3 = (TNR/TST)*alpha + 100*beta + P*gamma
         return scores1, scores2, scores3
+
 
 
 

SleepTranscription/SleepTranscription.py
@@ -213,14 +213,15 @@ class SleepTranscription:
         return [frame.object_present for frame in self.frame_list]
 
 
-    def detect_motion(self, n=5, threshold : float = 0.01) -> list:
+    def detect_motion(self, n=5) -> list:
         """
         Определяет движения объекта на видеоряде
         :param n: Количество кадров, на протяжении которых производится определение движений
         :param threshold: Доля изменившихся пикселей на кадре, при привышении которой будет зафиксировано движение
         :return:
         """
-        new_pixel_thresh = int(255*self.frame_list[0].image_size[0] * self.frame_list[0].image_size[1] * threshold)
+        pixel_count = int(255*self.frame_list[0].image_size[0] * self.frame_list[0].image_size[1])
+
         self.motion_images_list = list()
         self.motion_detection_list = list()
 
@@ -232,7 +233,7 @@ class SleepTranscription:
             for j in range(0, n_iter):
                 frame |= self.frame_list[i].mask ^ self.frame_list[i-j].mask
             self.motion_images_list.append(frame)
-            self.motion_detection_list.append(np.sum(frame) > new_pixel_thresh)
+            self.motion_detection_list.append(np.sum(frame)/pixel_count)
         return self.motion_detection_list
 
 
@@ -240,9 +241,13 @@ class SleepTranscription:
         sleep_model = SleepPoseClassifyer()
         obj_masks = list()
         for frame in self.frame_list:
-            if frame.object_present:
-                obj_masks.append(bf.get_64pix_mask(frame.mask))
+            obj_masks.append(bf.get_64pix_mask(frame.mask))
         self.pose_list = sleep_model.batch_classify(obj_masks)
+
+        obj_present = self.detect_object_frames()
+        for pose in range(len(self.pose_list)):
+            if not obj_present[pose]:
+                self.pose_list[pose] = -1
         save_frames_list(obj_masks, r"C:\Users\dboga\PycharmProjects\sleep quality using computer vision\Moving vid\Test_64")
         return self.pose_list
 
@@ -262,18 +267,18 @@ class SleepTranscription:
         self.do_ViBe_algorithm()
         self.blur_frames()
         self.detect_object_frames(thresh_object)
-        self.detect_motion(n, thresh_motion)
+        self.detect_motion(n)
         self.classify_poses()
 
         return self
 
 
-    def get_motion_frames(self) -> list:
+    def get_motion_frames(self, threshhold : float) -> list:
         """
         :return: возвращает список кадров, на которых было определено движение объекта
         """
         frames = list()
         for i in range(len(self.motion_detection_list)):
-            if self.motion_detection_list[i] & self.frame_list[i].object_present:
+            if (self.motion_detection_list[i] > threshhold) & self.frame_list[i].object_present:
                 frames.append(self.frame_list[i])
         return frames

basic_functions/basic_functions.py
@@ -31,20 +31,45 @@ def threshold(img, thresh):
 
 
 def get_64pix_mask(image):
-    image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-    _, _, stats, _ = cv2.connectedComponentsWithStats(image[:, :, 0], connectivity=4)
-    max_area_img = np.argmax(stats[1:, 4]) + 1
-    x, y, w, h, _ = stats[max_area_img]
+    _, _, stats, _ = cv2.connectedComponentsWithStats(image, connectivity=4)
+    max_area_img = np.argsort(-stats[1:, 4]) + 1
+
+    if len(max_area_img) == 0 or len(max_area_img) > 5:
+        image_64 = np.zeros((64, 64, 3), dtype=np.uint8)
+        return image_64
+
+
+    x, y, w, h, s = stats[max_area_img[0]]
     cx, cy = x + w // 2, y + h // 2
+    w,h = x+w, y+h
+
+    for i in max_area_img[1:3]:
+        xi, yi, wi, hi, si = stats[i]
+        coef = (si) / s
+        cxi, cyi = xi + wi // 2, yi + hi // 2
+        wi,hi = xi+wi,yi+hi
+        signxi = np.sign(cxi - cx)
+        signyi = np.sign(cyi - cy)
+
+        x_new = int(x + signxi * (xi // 2) * coef)
+        y_new = int(y + signyi * (yi // 2) * coef)
+        w_new = int(w + signxi * (wi // 2) * coef)
+        h_new = int(h + signyi * (hi // 2) * coef)
+
+        if x_new < x: x = x_new
+        if y_new < y: y = y_new
+        if w_new > w: w = w_new
+        if h_new > h: h = h_new
+
 
     margin = (w+h)//4
 
     lx = 0 if x-margin<0 else x-margin
     ly = 0 if y-margin<0 else y-margin
-    rx = image.shape[1] - 1 if x+w+margin>image.shape[1] else x+w+margin
-    ry = image.shape[0] - 1 if y+h+margin>image.shape[0] else y+h+margin
+    rx = image.shape[1] - 1 if w+margin>image.shape[1] else w+margin
+    ry = image.shape[0] - 1 if h+margin>image.shape[0] else h+margin
 
     image_64 = np.zeros((64, 64, 3), dtype=np.uint8)
-    image_64 = cv2.resize(image[ly:ry,lx:rx],(64,64),interpolation=cv2.INTER_LINEAR)
+    image_64 = cv2.resize(image[ly:ry,lx:rx],(64,64),interpolation=cv2.INTER_NEAREST)
 
-    return image_64
+    return cv2.cvtColor(image_64, cv2.COLOR_GRAY2RGB)